cca45d986508ce4a7b499077756587183105dcfc
Farhan Reza <108195448+farhan7reza7@users.noreply.github.com>
2024-02-18 20:07:42 +0530

    Create babel.config.jest.mjs

---
+++ babel.config.jest.mjs
@@ -0,0 +1,14 @@
+// babel.config.jest.js
+
+export default {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
