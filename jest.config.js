a4247c2e370ed9061a4216fae05c87cd5741e893
Farhan Reza <108195448+farhan7reza7@users.noreply.github.com>
2024-02-19 00:32:39 +0530

    Update jest.config.js  for delivery through CDN as well
    Update jest.config.js  for delivery through CDN as well

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
+  // Specify Babel options for Jest
+  babel: {
+    presets: ['@babel/preset-env'],
+    // other Babel options...
+  },
   testEnvironment: 'node',
 };
