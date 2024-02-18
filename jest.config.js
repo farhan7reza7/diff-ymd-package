16d5185e15be4a08e58d2d1267c60a73751474b9
Farhan Reza <108195448+farhan7reza7@users.noreply.github.com>
2024-02-19 00:55:57 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,6 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
-  // Specify Babel options for Jest
-  #babel: {
-   # presets: ['@babel/preset-env'],
-    // other Babel options...
-  #},
+  
   testEnvironment: 'node',
 };
